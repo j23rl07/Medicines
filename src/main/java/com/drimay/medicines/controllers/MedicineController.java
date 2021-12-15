6ea1fd05d42864bf16091b3bd437287bbda38036
Jaime Ramos Lara <j23rl07@gmail.com>
2021-12-15 12:00:10 +0100

    Example Entity created

---
+++ src/main/java/com/drimay/medicines/controllers/MedicineController.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.drimay.medicines.controllers;
+
+import com.drimay.medicines.models.Medicine;
+import com.drimay.medicines.services.MedicineService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author jaime
+ */
+
+@RestController
+@RequestMapping("/medicine")
+public class MedicineController {
+    
+    private static final Logger log = LoggerFactory.getLogger(MedicineController.class);
+    
+    @Autowired
+    private MedicineService medicineService;
+    
+    // GET method to fetch all medicines
+    @GetMapping()
+    public String getAllMedicines(ModelMap model) {
+        log.info("Mostrando todos los medicamentos (controlador)");
+        Iterable<Medicine> medicines = medicineService.findAll();
+        model.addAttribute("medicines", medicines);
+        return "";
+    }
+    
+    // GET method to fetch medicine by Id
+    @GetMapping("/details/{id}")
+    public String getMedicineById(@PathVariable(value = "id") Long Id, ModelMap model){
+        log.info("Mostrando medicamento por id (controlador)");
+        Medicine medicine = medicineService.findById(Id).get();
+        model.addAttribute("Medicine",medicine);
+        return "";
+    }
+    
+}
